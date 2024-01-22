2adb3f6bfdc9cf01ca88f54763a66123f28e341c
Gabriel Felipe de Oliveira <gabriel.investidor@outlook.com>
2024-01-22 16:14:19 -0300

    Popup finalizado

--- js/index.js
+++ js/index.js
@@ -6,12 +6,69 @@
 const button = document.getElementById("open-modal");
 const modal = document.getElementById("myModal");
 const buttonClosed = document.getElementById("closed-modal");
+const overlay = document.getElementById("overlay")
 button.onclick = function (){
     modal.showModal()
+    overlay.style.display = "block";
+
 }
+
 buttonClosed.onclick = function(){
     modal.close()
+    overlay.style.display = "none";
 }
+
+var selectOptionsDay = document.getElementById('selectOptionsDay');
+
+        // Adiciona opções dinamicamente de 1 a 20
+for (var i = 1; i <= 31; i++) {
+    var option = document.createElement('option');
+    option.id = i; 
+    option.value = i;
+    option.text = i;
+    selectOptionsDay.appendChild(option);
+}
+
+
+var selectOptionsMonth = document.getElementById('selectOptionsMonth');
+var meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+
+// Adiciona opções dinamicamente de 1 a 20
+for (var i = 0; i <= meses.length - 1; i++) {
+var option = document.createElement('option');
+option.id =  meses[i]; 
+option.value = meses[i];
+option.text = meses[i];
+selectOptionsMonth.appendChild(option);
+}
+
+var selectOptionsYears = document.getElementById('selectOptionsYears');
+
+        // Obtém o ano atual
+var anoAtual = new Date().getFullYear();
+
+// Adiciona opções dinamicamente para os anos de 1950 até o ano atual
+for (var ano = 1950; ano <= anoAtual; ano++) {
+    var option = document.createElement('option');
+    option.value = ano;
+    option.text = ano;
+    selectOptionsYears.appendChild(option);
+}
+
+var radioButtons = document.querySelectorAll('.popup-input-radio');
+        radioButtons.forEach(function(radioButton) {
+            radioButton.addEventListener('click', function() {
+                // Desmarca todos os outros botões de rádio quando um é clicado
+                radioButtons.forEach(function(otherRadioButton) {
+                    if (otherRadioButton !== radioButton) {
+                        otherRadioButton.checked = false;
+                    }
+                });
+            });
+        });
+
+
+
 
 // class Page {
 //     constructor(page){
